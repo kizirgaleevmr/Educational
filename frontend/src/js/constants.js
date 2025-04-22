5282ee45b634a8718b81490bdbe49eba436da891
Marsel Kizirgaleev <mkrmarsel@gmail.com>
2025-04-22 14:17:20 +0500

    Добавил глобальные константы

---
+++ frontend/src/js/constants.js
@@ -0,0 +1,8 @@
+/**
+ * Глобальные данные приложения.
+ * @property {string} api.apiUrl - URL базового API The Movie Database.
+ */
+export const ENV = {
+    apiUrl: "http://localhost:3000/",
+    endpoint: "students",
+};
