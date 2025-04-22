291914450256f97a167deecc37c304211ac748af
Marsel Kizirgaleev <mkrmarsel@gmail.com>
2025-04-22 14:08:05 +0500

    Добавили базовую структуру

---
+++ backend/src/dtos/student.dto.ts
@@ -0,0 +1,9 @@
+// Свойства, которые можно использовать для передачи данных по студентам
+export class StudentDto {
+  name?: string;
+  surname?: string;
+  groupName?: string;
+  address?: string;
+  age?: number;
+  
+}
